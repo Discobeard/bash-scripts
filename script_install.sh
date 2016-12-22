5324cde9feff56372858f4e59b0c456855c50ecb
alister mcconnell <alister.mcconnell@gmail.com>
2016-12-22 12:10:27 +0000

    adding git aliases

--- script_install.sh
+++ script_install.sh
@@ -4,14 +4,27 @@
 touch ~/.bash_profile
 touch ~/.bash_scripts
 
+setScriptAliases() {
+	echo "setting script aliases"
+	echo "alias kjps='$(pwd)/killalljps.sh'" >> ~/.bash_scripts
+	echo "alias rmscript='$(pwd)/script_remove.sh'" >> ~/.bash_scripts
+}
+
+setGitAliases() {
+	echo "setting git aliases"
+	 git config --global alias.st status
+	 git config --global alias.co checkout
+}
+
 if ! grep "~/.bash_scripts" ~/.bash_profile
 then
 	echo "source ~/.bash_scripts" >> ~/.bash_profile
 
 	echo "export SCRIPTLOC=$(pwd)" >> ~/.bash_scripts
 	echo ". $(pwd)/git-terminal/init.sh" >> ~/.bash_scripts
-	echo "alias kjps='$(pwd)/killalljps.sh'" >> ~/.bash_scripts
-	echo "alias rmscript='$(pwd)/script_remove.sh'" >> ~/.bash_scripts
+
+	setScriptAliases
+	setGitAliases
 
 else
 	echo "script already installed in the bash profile"
--- script_remove.sh
+++ script_remove.sh
@@ -1,2 +1,7 @@
+#!/bin/bash
+
 rm ~/.bash_scripts
 sed -i.bak '/bash_scripts/d' ~/.bash_profile
+
+git config --global --unset alias.st
+git config --global --unset alias.co
