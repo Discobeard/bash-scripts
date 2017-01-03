2960ac9d9fc5a91a2b272a18d8621b3d1e9b6d52
Discobeard <alister.mcconnell@gmail.com>
2017-01-03 17:21:25 +0000

    Create gls.sh

---
+++ gls.sh
@@ -0,0 +1,16 @@
+source "$SCRIPTLOC/git-terminal/git-terminal.sh"
+
+for i in $(ls -a)
+do
+
+	if [ -d $i ]
+	then	
+		if [ -d "./$i/.git" ]
+		then
+			cd $i
+			echo -e "$i $(branchname)"
+			cd ../
+		fi
+	fi
+
+done
